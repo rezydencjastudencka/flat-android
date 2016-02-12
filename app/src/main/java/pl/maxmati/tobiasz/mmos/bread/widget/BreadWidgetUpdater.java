08728567148edee0ccedfde23dbf2106740ba222
Marcin Moskal <mmos@student.agh.edu.pl>
2016-02-12 13:04:49 +0100

    Add bread notification (+localization strings)

--- app/src/main/java/pl/maxmati/tobiasz/mmos/bread/widget/BreadWidgetUpdater.java
+++ app/src/main/java/pl/maxmati/tobiasz/mmos/bread/widget/BreadWidgetUpdater.java
@@ -1,9 +1,12 @@
 package pl.maxmati.tobiasz.mmos.bread.widget;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.appwidget.AppWidgetManager;
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
@@ -21,10 +24,35 @@ public class BreadWidgetUpdater extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        int breadCount = getBreadCount();
         Log.d(TAG, "Got update request");
-        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
-        appWidgetManager.updateAppWidget(new ComponentName(this, BreadWidget.class), buildViews(getBreadCount()));
+
+        updateWidget(breadCount);
+        updateNotification(breadCount);
         return START_NOT_STICKY;
+    }
+
+    private void updateNotification(int breadCount) {
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        Notification.Builder mBuilder;
+
+        if(breadCount > 0) {
+            mNotificationManager.cancelAll();
+            Log.d(TAG, "Removing notification");
+            return;
+        }
+
+        mBuilder = new Notification.Builder(this).setSmallIcon(R.drawable.bread)
+                .setContentTitle(getString(R.string.notification_title))
+                .setContentText(getString(R.string.notification_body));
+
+        Log.d(TAG, "Creating notification");
+        mNotificationManager.notify(0, mBuilder.build());
+    }
+
+    private void updateWidget(int breadCount) {
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
+        appWidgetManager.updateAppWidget(new ComponentName(this, BreadWidget.class), buildViews(breadCount));
     }
 
     private RemoteViews buildViews(int breadCount) {
