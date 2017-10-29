3a356217398325e8672b7f5593aff615484bf36e
Radoslaw Pieja <pieja.radoslaw@gmail.com>
2017-10-29 15:54:05 +0100

    API Client work in progress

--- app/src/main/java/pl/rpieja/flat/SplashActivity.java
+++ app/src/main/java/pl/rpieja/flat/SplashActivity.java
@@ -1,17 +1,40 @@
 package pl.rpieja.flat;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.franmontiel.persistentcookiejar.ClearableCookieJar;
+import com.franmontiel.persistentcookiejar.PersistentCookieJar;
+import com.franmontiel.persistentcookiejar.cache.SetCookieCache;
+import com.franmontiel.persistentcookiejar.persistence.SharedPrefsCookiePersistor;
+
+import java.io.IOException;
+
+import okhttp3.CookieJar;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ClearableCookieJar cookieJar =
+                new PersistentCookieJar(new SetCookieCache(), new SharedPrefsCookiePersistor(this.getApplicationContext()));
+        FlatAPI flatAPI = new FlatAPI(cookieJar);
 
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
+//        try {
+//            if(flatAPI.validateSession()){
+//                Intent intent = new Intent(this, ChargesActivity.class);
+//                startActivity(intent);
+//                finish();
+//            }else{
+//                Intent intent = new Intent(this, MainActivity.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
     }
 }
