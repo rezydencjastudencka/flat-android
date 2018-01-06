6076ddd3af5af3a41317029a7b099f5906d9518a
Mateusz Nowotyński <maxmati@memleak.pl>
2018-01-06 18:22:05 +0100

    Add disabling and enabling new charge activity accept button

--- app/src/main/java/pl/rpieja/flat/viewmodels/NewChargeViewModel.java
+++ app/src/main/java/pl/rpieja/flat/viewmodels/NewChargeViewModel.java
@@ -1,5 +1,6 @@
 package pl.rpieja.flat.viewmodels;
 
+import android.arch.lifecycle.MediatorLiveData;
 import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
 import android.content.Context;
@@ -26,10 +27,37 @@ public class NewChargeViewModel extends ViewModel {
     public final MutableLiveData<Calendar> chargeDate = new MutableLiveData<>();
     public final MutableLiveData<String> chargeName = new MutableLiveData<>();
     public final MutableLiveData<String> chargeAmount = new MutableLiveData<>();
+    public final MediatorLiveData<Boolean> isValid = new MediatorLiveData<>();
+
 
     public NewChargeViewModel() {
         selectedUsers.setValue(new HashSet<>());
         chargeDate.setValue(Calendar.getInstance());
+
+        validate();
+        isValid.addSource(selectedUsers, value -> this.validate());
+        isValid.addSource(chargeName, value -> this.validate());
+        isValid.addSource(chargeAmount, value -> this.validate());
+
+    }
+
+    private void validate() {
+        if (selectedUsers.getValue() == null || selectedUsers.getValue().isEmpty()){
+            isValid.setValue(false);
+            return;
+        }
+
+        if (chargeName.getValue() == null || chargeName.getValue().isEmpty()){
+            isValid.setValue(false);
+            return;
+        }
+
+        if (chargeAmount.getValue() == null || chargeAmount.getValue().isEmpty()){
+            isValid.setValue(false);
+            return;
+        }
+
+        isValid.setValue(true);
     }
 
     public MutableLiveData<List<User>> getUsers() {
