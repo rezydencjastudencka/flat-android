cc388e92d82a4b26cd2be8941d220a6cfe5a027b
Marcin Moskal <mmos@memleak.pl>
2018-01-27 23:24:32 +0100

    Allow null savedInstanceState Bundle in DateDialog
    savedInstanceState defined as non-null causes method entry check failure,
    crashing the application.

--- app/src/main/java/pl/rpieja/flat/dialog/DateDialog.kt
+++ app/src/main/java/pl/rpieja/flat/dialog/DateDialog.kt
@@ -18,12 +18,12 @@ class DateDialog : DialogFragment(), DatePickerDialog.OnDateSetListener {
         this.listener = listener
     }
 
-    override fun onCreateDialog(savedInstanceState: Bundle): Dialog {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val c = Calendar.getInstance()
         val year = c.get(Calendar.YEAR)
         val month = c.get(Calendar.MONTH)
         val day = c.get(Calendar.DAY_OF_MONTH)
-        return DatePickerDialog(activity, this, year, month, day)
+        return DatePickerDialog(context, this, year, month, day)
     }
 
     override fun onDateSet(view: DatePicker, year: Int, month: Int, day: Int) {
