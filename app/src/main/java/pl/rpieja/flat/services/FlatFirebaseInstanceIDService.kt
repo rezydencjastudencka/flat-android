62f82f5eabed2d10f8aaf8bb04f630bc01cc403e
Mateusz Nowotyński <maxmati4@gmail.com>
2018-02-06 23:01:43 +0100

    Register to server after login
    Signed-off-by: Mateusz Nowotyński <maxmati4@gmail.com>

--- app/src/main/java/pl/rpieja/flat/services/FlatFirebaseInstanceIDService.kt
+++ app/src/main/java/pl/rpieja/flat/services/FlatFirebaseInstanceIDService.kt
@@ -1,6 +1,13 @@
 package pl.rpieja.flat.services
 
+import com.google.firebase.iid.FirebaseInstanceId
 import com.google.firebase.iid.FirebaseInstanceIdService
+import pl.rpieja.flat.api.FlatAPI
+
 
 class FlatFirebaseInstanceIDService : FirebaseInstanceIdService() {
+    override fun onTokenRefresh() {
+        val refreshedToken = FirebaseInstanceId.getInstance().token
+        FlatAPI.getFlatApi(this).registerFCM(refreshedToken!!)
+    }
 }
